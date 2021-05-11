87c4570caa9f57344c1a56446225dd99fc53d17c
Shivam Khandelwal <shivam.khandelwal@salesforce.com>
2021-05-11 07:35:51 +0530

    initial commit

--- src/main/java/com/mulesoft/demo/order/ConcentratorService.java
+++ src/main/java/com/mulesoft/demo/order/ConcentratorService.java
@@ -1,10 +1,22 @@
 package com.mulesoft.demo.order;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.el.stream.Optional;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -15,13 +27,32 @@ public class ConcentratorService {
 		SpringApplication.run(ConcentratorService.class, args);
 	}
 
-	@RequestMapping (value = "/concentrator", method = RequestMethod.POST)
+	@RequestMapping (value = "/concentrator", method = RequestMethod.GET)
 	@CrossOrigin
-	public OrderResponse orders () {
-		OrderResponse orderResponseExample = new OrderResponse();
+	public JSONArray concentrators (@RequestParam String city) {
+		
+	       //JSON parser object to parse read file
+           
+             JSONParser jsonParser = new JSONParser();
+             JSONArray concentratorList = null;
+             try (FileReader reader = new FileReader("concentrator.json"))
+             {
+             //Read JSON file
+             Object obj = jsonParser.parse(reader);
+             
+              concentratorList = (JSONArray) obj;
+ 
+             } catch (FileNotFoundException e) {
+             e.printStackTrace();
+             } catch (IOException e) {
+             e.printStackTrace();
+             } catch (ParseException e) {
+             e.printStackTrace();
+             }
+            
+		
 
-		orderResponseExample.setOrderResponseExample();
-
-		return orderResponseExample;
+		
+        return concentratorList;
 	}
 }
